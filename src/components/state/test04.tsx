aa80387e15f8f71cfd5fa2914cb09d2fd1b0c614
phument007 <phumentgaming@gmail.com>
2024-06-23 12:47:44 +0700

    updated state for latest example

--- src/components/state/test04.tsx
+++ src/components/state/test04.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import sculptureList from './app.tsx';
 const Gallery:React.FC = () => {
   const [index,setIndex] = useState(0);
+  const [showMore,setShowMore] = useState(true)
   const handleClick = () => {
     setIndex(index+1);
     if(index == sculptureList.length-1){
@@ -15,14 +16,24 @@ const Gallery:React.FC = () => {
        setIndex(sculptureList.length - 1);
     }
   }
+
+  const handleShow = () => {
+       setShowMore(!showMore)
+      
+  }
   let data = sculptureList[index];
   return (
     <div>
         <button onClick={handlePrev}>prev</button>
         <button onClick={handleClick}>Next</button>
         <p>{index + 1}/{sculptureList.length}</p>
-        <h3>{data.artist}</h3>
-        <p>{data.description}</p>
+        <h2>{data.artist}</h2>
+        <button onClick={handleShow}>
+           {(showMore? 'hide' : 'show')}
+        </button>
+        <p className='description'>
+           { showMore &&  <p>{data.description}</p> }
+        </p>
         <img style={{ width: '300px', height: '500px' }} src={data.url} alt={data.artist} />
     </div>
     
